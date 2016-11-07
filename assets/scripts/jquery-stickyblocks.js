2eb9b5e2464d9ccd8bd9097242e88d08fd7645d4
Bartosz Perończyk <bartosz@peronczyk.com>
2016-11-07 22:36:02 +0100

    Sticky blocks repair

--- assets/scripts/jquery-stickyblocks.js
+++ assets/scripts/jquery-stickyblocks.js
@@ -4,7 +4,8 @@
  *	JQ: STICKY BLOCKS
  *
  *	Script author	: Bartosz Perończyk (peronczyk.com)
- *	Date			: 2016-02-25
+ *	Created			: 2016-02-25
+ *	Modified		: 2016-11-07
  *
  *	--------------------------------------------------------------------------------
  *	DESCRIPTION:
@@ -102,11 +103,11 @@
 
 				if (blocks[i].columns) {
 					blocks[i].elem.find('th').each(function(j) {
-						blocks[i].columns[j].width = this.outerWidth();
+						blocks[i].columns[j].width = $(this).outerWidth();
 					});
 					if (blocks[i].clone) {
 						blocks[i].clone.find('th').each(function(j) {
-							this.css({'width': blocks[i].columns[j].width});
+							$(this).css({'width': blocks[i].columns[j].width});
 						});
 					}
 				}
@@ -117,15 +118,16 @@
 		// Method fired once when script is loaded
 		// Collects all sticky elements and sets their params in memory
 
-		init = function(config) {
-			if (this.length > 0) {
-				this.each(function(i) {
+		init = function() {
+
+			if ($(this).length > 0) {
+				$(this).each(function(i) {
 
 					// Set up block's basic params
 
 					blocks[i] = {
-						'elem'	: this,
-						'width'	: this.outerWidth(),
+						'elem'	: $(this),
+						'width'	: $(this).outerWidth(),
 					};
 
 					// If sticky element is a table header get widths of all of it's columns
@@ -134,8 +136,8 @@
 						blocks[i].columns = [];
 						blocks[i].elem.find('th').each(function(j) {
 							blocks[i].columns[j] = {
-								'elem'	: this,
-								'width'	: this.outerWidth()
+								'elem'	: $(this),
+								'width'	: $(this).outerWidth()
 							};
 						});
 					}
@@ -144,7 +146,7 @@
 				$document.on('scroll', function() { window.requestAnimationFrame(onScroll); });
 				$window.on('resize', function() { requestAnimationFrame(onResize); });
 
-				return this;
+				return $(this);
 			}
 			else if (config.debug) console.error('jQ.StickyBlocks: no sticky elements selected');
 		};
@@ -158,16 +160,13 @@
 
 		var
 			// Setup configuration
-			config = $.extend({}, defaults, options),
-
-			// Definitions
-			_self = $(this);
+			config = $.extend({}, defaults, options);
 
 		if (config.debug) console.info('Plugin loaded: stickyBlocks');
 
-		init.apply(_self, config);
+		init.apply(this, config);
 
-		return _self;
+		return $(this);
 	}
 
 })(jQuery);
