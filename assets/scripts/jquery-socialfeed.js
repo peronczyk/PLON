d0b765cabddd89da18cd94f74b56950d31cd3f76
Bartosz Perończyk <bartosz@peronczyk.com>
2017-02-02 22:33:13 +0100

    Add files

--- assets/scripts/jquery-socialfeed.js
+++ assets/scripts/jquery-socialfeed.js
@@ -54,6 +54,10 @@
 			// Class name or ID of entry
 			'entryElem': null,
 
+			// Data attribute name that connects element with text that should be
+			// inserted to it
+			'entryElementsSelector': 'data-entry-element',
+
 			// Class name or ID of previous button
 			'btnPrevious': null,
 
@@ -66,17 +70,51 @@
 			'errorClassName'		: 'is-Error',
 			'hasNextClassName'		: 'has-Next',
 			'hasPreviousClassName'	: 'has-Previous',
-		},
+		};
 
-		services = {
+
+	/*	----------------------------------------------------------------------------
+	 *	SERVICES CONFIGURATION AND METHODS
+	 *
+	 *	@url - API endpoint URL
+	 *	@defaultFields - String that contains list of variables to be received with
+	 *		each of feed posts (eg.: title, photo, date, etc)
+	 *	@getDataValues - Method that return universal element names
+	 *		with parsed values bind to them
+	 */
+
+	var services = {
+
+			// Facebook API
 			'facebook': {
 				'url': 'https://graph.facebook.com/v2.8/{sourceID}/posts?fields={fields}&limit={postsPerPage}&access_token={accessToken}',
-				'defaultFields': 'message,story,full_picture,picture,likes.summary(true).limit(0),comments.summary(true).limit(0),permalink_url,link',
+				'defaultFields': 'message,created_time,story,full_picture,picture,likes.summary(true).limit(0),comments.summary(true).limit(0),permalink_url,link',
+				'dataVariable': 'data',
+				'getDataValues': function(receivedData) {
+					if (!receivedData.data || receivedData.data.length < 1) return false;
+					var values = [];
+					for (var index in receivedData.data) {
+						values[index] = {
+							'link'		: receivedData.data[index].permalink_url,
+							'date'		: formatDate(receivedData.data[index].created_time),
+							'cover'		: receivedData.data[index].full_picture,
+							'text'		: receivedData.data[index].message,
+							'likes'		: (receivedData.data[index].likes) ? receivedData.data[index].likes.summary.total_count : 0,
+							'comments'	: (receivedData.data[index].comments) ? receivedData.data[index].comments.summary.total_count : 0
+						};
+					};
+					return values;
+				},
 			},
 
+			// YouTube API
 			'youtube': {
 				'url': 'https://www.googleapis.com/youtube/v3/search?channelId={sourceID}&order=date&part={fields}&maxResults={postsPerPage}&key={accessToken}',
-				'defaultFields': 'snippet'
+				'defaultFields': 'snippet',
+				'getDataValues': function(receivedData) {
+					if (!receivedData.items || receivedData.items.length < 1) return false;
+					return [];
+				}
 			},
 
 			'twitter': {
@@ -92,6 +130,70 @@
 			.replace('{postsPerPage}', config.postsPerPage);
 	}
 
+	// Get DOM entry element and search child elements specified by data selector
+	var prepareEntryElements = function($entry, selector) {
+		var entryElements = [];
+		$entry.find('[' + selector + ']').each(function(i, elem) {
+			entryElements[$(this).attr(selector)] = $(this);
+		});
+		return entryElements;
+	}
+
+	// Format date: YYYY-MM-DD HH:MM
+	var formatDate = function(date) {
+		var date = new Date(date);
+		return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes();
+	}
+
+
+	/*	----------------------------------------------------------------------------
+	 *	GET FEED ENTRIES
+	 */
+
+	var getFeedEntries = function(config, $entriesWrapper, $entry, entryElements) {
+		var preparedUrl = prepareUrl(config),
+			result = $.ajax({
+				'url': preparedUrl
+			});
+
+		result.then(
+
+			// Success
+			function(data, textStatus, jqXHR) {
+				if (config.debug) console.log('socialFeed: Data received succesully from: ' + preparedUrl);
+
+				var dataValues = services[config.service].getDataValues(jqXHR.responseJSON);
+
+				if (dataValues === false) {
+					if (config.debug) console.warn('socialFeed: Could not process received data. Data is empty or broken.');
+					return;
+				}
+
+				// Prepare entries and insert them into entries wrapper
+				for (var index in dataValues) {
+					for (var type in entryElements) {
+						if (dataValues[index][type]) {
+							if (entryElements[type].is('a'))
+								entryElements[type].attr('href', dataValues[index][type]);
+
+							else if (entryElements[type].is('img'))
+								entryElements[type].attr('src', dataValues[index][type]);
+
+							else entryElements[type].html(dataValues[index][type]);
+						}
+					}
+					$entriesWrapper.append($entry.clone());
+				};
+			},
+
+			// Error
+			function(test) {
+				console.log('ERROR');
+				console.log(test);
+			}
+		);
+	}
+
 
 	/*	----------------------------------------------------------------------------
 	 *	SET UP JQUERY PLUGIN
@@ -104,22 +206,27 @@
 			config = $.extend({}, defaults, options),
 
 			// Definitions
-			$self = $(this),
-			$btnPrevious, $btnNext;
+			$self = $(this);
 
-		if (config.debug) console.info('Plugin loaded: socialFeed, selected service: ' + config.service);
+		if (config.debug) console.info('Plugin loaded: socialFeed [' + config.service + ']');
 
-		var preparedUrl = prepareUrl(config);
+		if (!config.service || !services[config.service]) {
+			if (config.debug) console.warn('socialFeed: Selected service "' + config.service + '" is not supported');
+			return;
+		}
 
-		var result = $.ajax({
-			'url'		: preparedUrl,
-			'method'	: 'get'
-		});
+		if ($self.length < 1) {
+			if (config.debug) console.warn('socialFeed: Wrapper element not found');
+			return;
+		}
 
-		result
-			.success(function(data) {
-				console.log(data);
-			});
+		var $entry = $self.find(config.entryElem),
+			$entriesWrapper = $entry.parent(),
+			entryElements = prepareEntryElements($entry, config.entryElementsSelector);
+
+		$entry.detach();
+
+		getFeedEntries(config, $entriesWrapper, $entry, entryElements);
 
 		return $self;
 	}
--- examples/SocialFeed/custom.js
+++ examples/SocialFeed/custom.js
@@ -9,7 +9,6 @@ $(function() {
 		'service'		: 'facebook',
 		'sourceID'		: 'peronczyk',
 		'accessToken'	: '1800666566886889|ca3aebed50bb3a122d0e44595ff5b405',
-		'fields'		: 'message,story,full_picture,picture,likes.summary(true).limit(0),comments.summary(true).limit(0),permalink_url,link',
 		'entryElem'		: '#facebook-feed__entry',
 		'btnPrevious'	: '.c-Feed__prev',
 		'btnNext'		: '.c-Feed__next',
@@ -18,12 +17,11 @@ $(function() {
 
 	// YouTube movies
 
-	$('#twitter-feed').socialFeed({
+	$('#youtube-feed').socialFeed({
 		'debug'			: debug,
 		'service'		: 'youtube',
 		'sourceID'		: 'UCXQ1gyZTgWYkL1PWrX3jiQQ',
 		'accessToken'	: 'AIzaSyAVboOKc2owBE-5QzuNAV1I3nbypylT2Nc',
-		'fields'		: '',
 		'entryElem'		: '#youtube-feed__entry',
 		'btnPrevious'	: '.c-Feed__prev',
 		'btnNext'		: '.c-Feed__next',
