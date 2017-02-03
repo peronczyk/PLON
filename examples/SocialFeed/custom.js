380008db5bc616a3c4f074a45ebc81c08e2d48d3
Bartosz Perończyk <bartosz@peronczyk.com>
2017-02-03 21:27:28 +0100

    Almost working

--- examples/SocialFeed/custom.js
+++ examples/SocialFeed/custom.js
@@ -7,7 +7,7 @@ $(function() {
 	$('#facebook-feed').socialFeed({
 		'debug'			: debug,
 		'service'		: 'facebook',
-		'sourceID'		: 'peronczyk',
+		'sourceId'		: 'peronczyk',
 		'accessToken'	: '1800666566886889|ca3aebed50bb3a122d0e44595ff5b405',
 		'entryElem'		: '#facebook-feed__entry',
 		'btnPrevious'	: '.c-Feed__prev',
@@ -20,7 +20,7 @@ $(function() {
 	$('#youtube-feed').socialFeed({
 		'debug'			: debug,
 		'service'		: 'youtube',
-		'sourceID'		: 'UCXQ1gyZTgWYkL1PWrX3jiQQ',
+		'sourceId'		: 'UCL8ZULXASCc1I_oaOT0NaOQ',
 		'accessToken'	: 'AIzaSyAVboOKc2owBE-5QzuNAV1I3nbypylT2Nc',
 		'entryElem'		: '#youtube-feed__entry',
 		'btnPrevious'	: '.c-Feed__prev',
