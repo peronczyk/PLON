879e74b239ddcd3b8f307d6bf89e781774733b7e
Bartosz Perończyk <bartosz@peronczyk.com>
2017-04-04 08:36:06 +0200

    Script name change

--- assets/scripts/plon.socialfeed.js
+++ assets/scripts/plon.socialfeed.js
@@ -23,10 +23,6 @@
  *	================================================================================
  */
 
-/*
-	global jQuery
-*/
-
 
 (function($) {
 
@@ -91,11 +87,7 @@
 	var formatDate = function(sourceDate) {
 		var date = new Date(sourceDate);
 		return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes();
-<<<<<<< HEAD:assets/scripts/jquery.socialfeed.js
 	};
-=======
-	}
->>>>>>> 826edeee13ae057d3b5cc4e029c1b63e573fee45:assets/scripts/jquery-socialfeed.js
 
 
 	/*	----------------------------------------------------------------------------
@@ -122,19 +114,11 @@
 					return 'https://graph.facebook.com/v2.8/' + config.sourceId + '/posts?fields=' + this.defaultFields + '&limit=' + config.postsPerPage + '&access_token=' + config.accessToken;
 				},
 
-<<<<<<< HEAD:assets/scripts/jquery.socialfeed.js
 				urlPrev: function(config, receivedData) {
 					return receivedData.paging.next ? receivedData.paging.next : false;
 				},
 
 				urlNext: function(config, receivedData) {
-=======
-				'urlPrev': function(config, receivedData) {
-					return receivedData.paging.next ? receivedData.paging.next : false;
-				},
-
-				'urlNext': function(config, receivedData) {
->>>>>>> 826edeee13ae057d3b5cc4e029c1b63e573fee45:assets/scripts/jquery-socialfeed.js
 					return receivedData.paging.previous ? receivedData.paging.previous : false;
 				},
 
@@ -144,21 +128,12 @@
 						var feedList = receivedData.data;
 						for (var i in feedList) {
 							values[i] = {
-<<<<<<< HEAD:assets/scripts/jquery.socialfeed.js
 								link		: feedList[i].permalink_url,
 								date		: formatDate(feedList[i].created_time),
 								image		: feedList[i].full_picture,
 								text		: feedList[i].message,
 								likes		: feedList[i].likes ? feedList[i].likes.summary.total_count : 0,
 								comments	: feedList[i].comments ? feedList[i].comments.summary.total_count : 0
-=======
-								'link'		: feedList[i].permalink_url,
-								'date'		: formatDate(feedList[i].created_time),
-								'image'		: feedList[i].full_picture,
-								'text'		: feedList[i].message,
-								'likes'		: feedList[i].likes ? feedList[i].likes.summary.total_count : 0,
-								'comments'	: feedList[i].comments ? feedList[i].comments.summary.total_count : 0
->>>>>>> 826edeee13ae057d3b5cc4e029c1b63e573fee45:assets/scripts/jquery-socialfeed.js
 							};
 						}
 					}
@@ -219,11 +194,7 @@
 			entryElements[$(this).attr(selector)] = $(this);
 		});
 		return entryElements;
-<<<<<<< HEAD:assets/scripts/jquery.socialfeed.js
 	};
-=======
-	}
->>>>>>> 826edeee13ae057d3b5cc4e029c1b63e573fee45:assets/scripts/jquery-socialfeed.js
 
 
 	/*	----------------------------------------------------------------------------
@@ -255,11 +226,7 @@
 		$self.addClass(config.classNames.loading);
 
 		var preparedUrl = url ? url : services[config.service].url(config),
-<<<<<<< HEAD:assets/scripts/jquery.socialfeed.js
 			result = $.ajax({url: preparedUrl});
-=======
-			result = $.ajax({'url': preparedUrl});
->>>>>>> 826edeee13ae057d3b5cc4e029c1b63e573fee45:assets/scripts/jquery-socialfeed.js
 
 		result.then(
 
